22fc8f028a6faeefaf77dc7f59c18dc4fadeead9
Sherah Smith <ssmith@wikimedia.org>
2014-02-13 13:26:17 -0800

    fix args issues and make pie chart dynamic by allowing specification of data url

--- FundraisingChart/fr_chart_body.php
+++ FundraisingChart/fr_chart_body.php
@@ -3,37 +3,43 @@ class FundraisingChart {
 	static function onParserInit( Parser $parser ) {
 		$parser->setHook( 'fundraisingChart', array(__CLASS__, 'frChartRender') );
 	}
-    static function frChartDataSetFetch( $chartType, $dataSet ){
+    static function frChartDataSetFetch( $dataset ){
         //testing
-        $ret = '<p>' . $chartType . " and " . $dataSet . '</p>';
-        return $ret;
+        $raw_json = file_get_contents($dataset);
+        $d = json_decode($raw_json);
+
+        return json_encode($d);
     }
 	static function frChartRender( $input, array $args, Parser $parser, PPFrame $frame ) {
         $parser -> getOutput()->addModules('ext.fundraisingChart');
 
         //according to the specified chart type, validate and present specified dataSet.
-        //todo: fix issue with array?
-        //$ret .= FundraisingChart::frChartDataSetFetch( $args['chartType'], $args['dataSet']);
-        //$ret  = '<p> here is the chart type: ' . $args['chartType'] . '</p>';
+        $dataset = FundraisingChart::frChartDataSetFetch( $args['dataset'] );
 
-        $ret .= '<table class="pieChartTable">';
 
-        //set the title, which is the first argument given as a tag attribute
-        $ret .= '<tr><td colspan="2"><h1>' . $args['title'] . '</h1></td></tr>';
+        if($args['charttype'] === 'pie-chart'){
+            $ret = '<div id="pieChartArea" data-chartdata=' . $dataset . '>';
+            $ret .= '<table class="pieChartTable">';
+            //set the title, which is the first argument given as a tag attribute
+            $ret .= '<tr><td colspan="2"><h1>' . $args['title'] . '</h1></td></tr>';
 
-        $ret .= '<tr>';
-        $ret .= '<td>';
+            $ret .= '<tr>';
+            $ret .= '<td>';
 
-        //todo: fix chart id clashing possibility by passing in indexes or something
-		$ret .= '<canvas id="pieChart" height="350" width="350" margin-right="10"></canvas>';
-        $ret .= '</td>';
+            //todo: fix chart id clashing possibility by passing in indexes or something
+            $ret .= '<canvas id="pieChart" height="350" width="350" margin-right="10"></canvas>';
+            $ret .= '</td>';
 
-        $ret .= '<td>';
-		$ret .= '<div id="pieChartFilter"></div>';
-        $ret .= '</td>';
-        $ret .= '</tr>';
+            $ret .= '<td>';
+            $ret .= '<div id="pieChartFilter"></div>';
+            $ret .= '</td>';
+            $ret .= '</tr>';
 
-		$ret .= '</table>';
+            $ret .= '</table>';
+            $ret .= '</div>'; //close the chart type area.
+        }
+
+
 		return $ret;
 	}
 }
