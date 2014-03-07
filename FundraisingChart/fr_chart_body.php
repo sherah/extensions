2ded2ec4d5c5cfd5458aa7bece5259c5381b3d51
Sherah Smith <ssmith@wikimedia.org>
2014-03-07 09:53:42 -0800

    fix fr_chart_body by drying up chart display function and using commenting conventions per feedback

--- FundraisingChart/fr_chart_body.php
+++ FundraisingChart/fr_chart_body.php
@@ -16,17 +16,51 @@
  *
  */
 
-
+/**
+ * Class FundraisingChart
+ *
+ *
+ * Usage for the tag <fundraisingChart />:
+ *
+ * charttype: pie-chart, line-chart, or bar-chart
+ * dataset: a URL that provides the JSON in the correct format for the chart.
+ * the formats:
+ * pie-chart:  json with count, uses the "count" field to generate the pie slices. so use counts.
+ * make sure each count is sorted by month.
+ * bar-chart:  json with 'date' x-axis and 'count' y-axis.
+ * line-chart: json with date x-axis and total y-axis.
+ * map-chart:  json map data in the datamaps format, usually with fillColor and associated data.
+ * title: the title that will display at the top of this chart.
+ * 
+ */
 class FundraisingChart {
     static function onParserInit( Parser $parser ) {
         $parser->setHook( 'fundraisingChart', array(__CLASS__, 'frChartRender') );
     }
+
+    /**
+     * Fetch the data to be displayed in the charts.
+     *
+     * todo: make safer by mapping URIs to URLs
+     * @param $dataset
+     * @return string
+     */
     static function frChartDataSetFetch( $dataset ){
         $raw_json = file_get_contents($dataset);
         $d = json_decode($raw_json);
 
         return json_encode($d);
     }
+
+    /**
+     * Display charts
+     *
+     * @param $input
+     * @param array $args
+     * @param Parser $parser
+     * @param PPFrame $frame
+     * @return string
+     */
     static function frChartRender( $input, array $args, Parser $parser, PPFrame $frame ) {
         $parser -> getOutput()->addModules('ext.fundraisingChart');
 
@@ -36,15 +70,15 @@ class FundraisingChart {
             $dataset = "nothing";
         };
 
-        if($args['charttype'] === 'pie-chart'){
-
+        $chartStyle = substr($args['charttype'], 0, -6);
+        if($chartStyle !== 'map') {
             //create a unique id for the chart div in case multiple pie charts appear on the same page.
             $title = str_replace(' ', '', $args['title']);
 
             //associated chart data goes into its own attribute for javascript to listen to.
-            $ret = '<div id="pieChartArea' . $title . '" class="pieArea" data-chartdata=' . $dataset . '>';
+            $ret = '<div id="' . $chartStyle . 'ChartArea' . $title . '" class="' . $chartStyle . 'Area" data-chartdata=' . $dataset . '>';
 
-            $ret .= '<table class="pieChartTable">';
+            $ret .= '<table class="' . $chartStyle . 'ChartTable">';
 
             //set title.
             $ret .= '<tr><td colspan="2"><h1>' . $args['title'] . '</h1></td></tr>';
@@ -52,11 +86,11 @@ class FundraisingChart {
             $ret .= '<tr>';
             $ret .= '<td>';
 
-            $ret .= '<canvas id="pieChart' . $title . '" class="pieCanvas" height="350" width="350" margin-right="10"></canvas>';
+            $ret .= '<canvas id="' . $chartStyle . 'Chart' . $title . '" class="' . $chartStyle . 'Canvas" height="400" width="650" margin-right="10"></canvas>';
             $ret .= '</td>';
 
             $ret .= '<td>';
-            $ret .= '<div id="pieChart' . $title . 'Filter" class="pieFilter"></div>';
+            $ret .= '<div id="' . $chartStyle . 'Chart' . $title . 'Filter" class="' . $chartStyle . 'Filter"></div>';
             $ret .= '</td>';
             $ret .= '</tr>';
 
@@ -64,65 +98,14 @@ class FundraisingChart {
             $ret .= '</div>'; //close the chart type area.
 
         }
-        if($args['charttype'] === 'bar-chart'){
-
-            $title = str_replace(' ', '', $args['title']);
-
-            $ret = '<div id="barChartArea' . $title . '" class="barArea" data-chartdata=' . $dataset . '>';
-
-            $ret .= '<table class="barChartTable">';
-
-            //set title.
-            $ret .= '<tr><td colspan="2"><h1>' . $args['title'] . '</h1></td></tr>';
-
-            $ret .= '<tr>';
-            $ret .= '<td>';
-
-            $ret .= '<canvas id="barChart' . $title . '" class="barCanvas" height="400" width="650" margin-right="10"></canvas>';
-            $ret .= '</td>';
-
-            $ret .= '<td>';
-            $ret .= '<div id="barChart' . $title . 'Filter" class="barFilter"></div>';
-            $ret .= '</td>';
-            $ret .= '</tr>';
-
-            $ret .= '</table>';
-            $ret .= '</div>';
-        }
-        if($args['charttype'] === 'map-chart'){
+        else {
 
             $title = str_replace(' ', '', $args['title']);
 
             $ret = '<div id="mapChartArea' . $title . '" class="mapArea" data-chartdata=' . $dataset . '>';
-            $ret .= '<p>' . $dataset . 'data </p>';
 
             //set title.
             $ret .= '<h1>' . $args['title'] . '</h1></td></tr>';
-            $ret .= '</div>';
-        }
-        if($args['charttype'] === 'line-chart'){
-
-            $title = str_replace(' ', '', $args['title']);
-
-            $ret = '<div id="lineChartArea' . $title . '" class="lineArea" data-chartdata=' . $dataset . '>';
-
-            $ret .= '<table class="lineChartTable">';
-
-            //set title.
-            $ret .= '<tr><td colspan="2"><h1>' . $args['title'] . '</h1></td></tr>';
-
-            $ret .= '<tr>';
-            $ret .= '<td>';
-
-            $ret .= '<canvas id="lineChart' . $title . '" class="lineCanvas" height="400" width="650" margin-right="10"></canvas>';
-            $ret .= '</td>';
-
-            $ret .= '<td>';
-            $ret .= '<div id="lineChart' . $title . 'Filter" class="lineFilter"></div>';
-            $ret .= '</td>';
-            $ret .= '</tr>';
-
-            $ret .= '</table>';
             $ret .= '</div>';
         }
 
@@ -130,14 +113,4 @@ class FundraisingChart {
     }
 }
 
-//Usage for the tag <fundraisingChart />:
 
-//charttype: pie-chart, line-chart, or bar-chart
-//dataset: a URL that provides the JSON in the correct format for the chart.
-//the formats:
-//pie-chart:  json with count, uses the "count" field to generate the pie slices. so use counts.
-//make sure each count is sorted by month.
-//bar-chart:  json with 'date' x-axis and 'count' y-axis.
-//line-chart: json with date x-axis and total y-axis.
-//map-chart:  json map data in the datamaps format, usually with fillColor and associated data.
-//title: the title that will display at the top of this chart.
